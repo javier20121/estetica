c6330ee9f9eee0a25c063dc2cff8625a8af00301
javier <beckerjavier12@gmail.com>
2026-02-23 12:42:30 -0300

    Actualizar URL del Web App de Apps Script en script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // URL del Web App de Apps Script (Implementacion -> URL).
-const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbz3qmMmnvtJyw-cuUHXLi0fbOWJjDr1CkgFcaj70nWJtlwrK6PWovLjp4P5pc9Q4O5W/exec';
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbw47qOKv7HqPA-Igurkk6iRqbvrGGxRP5LOCpaqlv65toPYw2fpvadAZeGvmjgM3wEl/exec';
 
 function handleLogin(e) {
 	e.preventDefault();
